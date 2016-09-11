b0ef9914c1e0357ce3b052ac5686976c28ea59db
illingtonFlex <jason.norman@gmail.com>
2016-09-10 23:16:21 -0500

    Removing useless annotations.

--- src/main/java/jcn/deduce/server/mongo/DeduceMatchRepository.java
+++ src/main/java/jcn/deduce/server/mongo/DeduceMatchRepository.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@SpringBootApplication
-@EnableMongoRepositories
 public interface DeduceMatchRepository extends MongoRepository<DeduceMatch, String>
 {
     DeduceMatch findById(String id);
