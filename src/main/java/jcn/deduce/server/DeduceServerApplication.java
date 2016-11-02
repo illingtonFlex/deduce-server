b7df809c1cd01e4abbe8b4843eeffa42c249bc5a
illingtonFlex <jason.norman@gmail.com>
2016-11-01 20:24:32 -0500

    Switching to executable/deployable war.

--- src/main/java/jcn/deduce/server/DeduceServerApplication.java
+++ src/main/java/jcn/deduce/server/DeduceServerApplication.java
@@ -2,11 +2,18 @@ package jcn.deduce.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoRepositories
-public class DeduceServerApplication {
+public class DeduceServerApplication  extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DeduceServerApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DeduceServerApplication.class, args);
