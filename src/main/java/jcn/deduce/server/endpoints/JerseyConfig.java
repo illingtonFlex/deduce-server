f2f26183454ece4117d800ccc7066a154ba0f012
illingtonFlex <jason.norman@gmail.com>
2017-12-19 12:32:07 -0600

    Enabling logging.

--- src/main/java/jcn/deduce/server/endpoints/JerseyConfig.java
+++ src/main/java/jcn/deduce/server/endpoints/JerseyConfig.java
@@ -2,15 +2,21 @@ package jcn.deduce.server.endpoints;
 
 import jcn.deduce.server.model.JacksonObjectMapperProvider;
 import org.glassfish.jersey.jackson.JacksonFeature;
+import org.glassfish.jersey.logging.LoggingFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.ServerProperties;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Configuration
 public class JerseyConfig extends ResourceConfig
 {
     public JerseyConfig()
     {
+        register(new LoggingFeature(Logger.getLogger(LoggingFeature.DEFAULT_LOGGER_NAME), Level.INFO,
+                LoggingFeature.Verbosity.PAYLOAD_ANY, Integer.MAX_VALUE));
         register(Create.class);
         register(ListWords.class);
         register(LetterAtIndex.class);
